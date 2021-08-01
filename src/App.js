b5f3a95a0b8242f3eed1af31617841b6be4d008c
shushu <minsoo59@naver.com>
2021-08-01 14:31:40 +0900

    go

---
+++ src/App.js
@@ -0,0 +1,100 @@
+function App() {
+  return (
+    <>
+      <header>
+        <div>
+          Donica Ida is an art director and designer working in editorial,
+          identity, and digital design.
+        </div>
+      </header>
+      <main>
+        <div className="portfolio__menu">
+          <span>Year</span>
+          <span>Client</span>
+          <span>Project</span>
+        </div>
+        <div className="portfolio__items">
+          <span>2019</span>
+          <span>Cayas</span>
+          <span>Identity, website</span>
+        </div>{" "}
+        <div className="portfolio__items">
+          <span>2019</span>
+          <span>Cayas</span>
+          <span>Identity, website</span>
+        </div>{" "}
+        <div className="portfolio__items">
+          <span>2019</span>
+          <span>Cayas</span>
+          <span>Identity, website</span>
+        </div>{" "}
+        <div className="portfolio__items">
+          <span>2019</span>
+          <span>Cayas</span>
+          <span>Identity, website</span>
+        </div>
+        <div className="portfolio__items">
+          <span>2019</span>
+          <span>Cayas</span>
+          <span>Identity, website</span>
+        </div>{" "}
+        <div className="portfolio__items">
+          <span>2019</span>
+          <span>Cayas</span>
+          <span>Identity, website</span>
+        </div>
+        <div className="portfolio__items">
+          <span>2019</span>
+          <span>Cayas</span>
+          <span>Identity, website</span>
+        </div>
+        <div className="portfolio__items">
+          <span>2019</span>
+          <span>Cayas</span>
+          <span>Identity, website</span>
+        </div>
+        <div className="portfolio__items">
+          <span>2019</span>
+          <span>Cayas</span>
+          <span>Identity, website</span>
+        </div>
+        <div className="portfolio__items">
+          <span>2019</span>
+          <span>Cayas</span>
+          <span>Identity, website</span>
+        </div>
+        <div className="portfolio__items">
+          <span>2019</span>
+          <span>Cayas</span>
+          <span>Identity, website</span>
+        </div>
+      </main>
+      <div className="education">
+        <span>Education</span> MFA Design Entrepreneurship, School of Visual
+        Arts; BFA Visual Communication Design, University of Washington.
+        <span>Formerly at</span> HuffPost Highline, Mary Review, Critical Mass,
+        Pentagram. <span>Select clients</span> Airbnb, Condé Nast Traveler,
+        Google Creative Lab, Planned Parenthood.
+      </div>
+      <div className="about">
+        <span>About</span> Donica Ida is a Hawaii-born designer who loves
+        beautiful typography and a well-told story. She is the former Creative
+        Director of HuffPost Highline and Design Director of Mary Review. Donica
+        lives somewhere between Brooklyn and the PNW, and splits her spare time
+        between road trips, <a href="#">hikes</a> and ramen. She is currently
+        contracting at Google Creative Lab while looking for new opportunities*.
+        <span>Connect</span> <a href="#">Email</a>, <a href="#">Instagram</a>,
+        <a href="#">LinkedIn</a>, <a href="#">Working</a>
+        <a href="#">NotWorking</a>.
+      </div>
+      <footer>
+        <div>
+          *{new Date().getFullYear()} portfolio update coming shortly. Email for
+          latest work. Mahalo.
+        </div>
+      </footer>
+    </>
+  );
+}
+
+export default App;
